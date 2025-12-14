86dd4e5aeb4274cde8bca18f3e2eddf13ff5ae99
Kahilo Massango <kahilomassango@gmail.com>
2025-12-14 09:01:18 +0100

    Remove sensitive client ID and secret from AppConstants
    Removed sensitive WEB_CLIENT_ID and WEB_CLIENT_SECRET values.

--- composeApp/src/commonMain/kotlin/org/example/homeflow/AppConstants.kt
+++ composeApp/src/commonMain/kotlin/org/example/homeflow/AppConstants.kt
@@ -1,6 +1,5 @@
 package org.example.homeflow
 
 object AppConstants {
-    const val WEB_CLIENT_ID  = "756641327702-i190batvamvnmoo7dlrdfk2g2ut63rih.apps.googleusercontent.com"
-    const val WEB_CLIENT_SECRET  = "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"
+    const val WEB_CLIENT_ID  = ""
 }
